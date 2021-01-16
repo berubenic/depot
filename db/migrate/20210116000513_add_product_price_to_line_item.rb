2b7f9c926115a34e03fd38252c156de6ebeff74e
Nicholas <berubenic@gmail.com>
2021-01-16 13:14:21 -0500

    destroy individual line_item

--- db/migrate/20210116000513_add_product_price_to_line_item.rb
+++ db/migrate/20210116000513_add_product_price_to_line_item.rb
@@ -2,7 +2,8 @@ class AddProductPriceToLineItem < ActiveRecord::Migration[6.1]
     def change
         add_column :line_items, :price, :decimal, precision: 8, scale: 2
         # update 'UPDATE line_items SET price = (SELECT products.price FROM products WHERE line_items.product_id = products.id)'
-        LineItem.update_all('price = (SELECT products.price FROM products WHERE line_items.product_id = products.id)')
+        sql = 'UPDATE line_items SET price = (SELECT products.price FROM products WHERE line_items.product_id = products.id)'
+        ActiveRecord::Base.connection.execute(sql)
     end
 end
 # UPDATE lineItems SET lineItems.price = product.price
