6c659c3209ca7fea9da0173cfe57a7c455555105
Nicholas <berubenic@gmail.com>
2021-01-20 18:13:34 -0500

    Chapter 13 playime

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -31,9 +31,53 @@ class OrdersTest < ApplicationSystemTestCase
         fill_in 'order_email', with: 'dave@example.com'
 
         assert_no_selector '#order_routing_number'
+        assert_no_selector '#order_account_number'
 
         select 'Check', from: 'Pay type'
 
         assert_selector '#order_routing_number'
+        assert_selector '#order_account_number'
+    end
+
+    test 'check credit card number' do
+        visit store_index_url
+
+        click_on 'Add to Cart', match: :first
+
+        click_on 'Checkout'
+
+        fill_in 'order_name', with: 'Dave Thomas'
+        fill_in 'order_address', with: '123 Main Street'
+        fill_in 'order_email', with: 'dave@example.com'
+
+        assert_no_selector '#order_credit_card_number'
+        assert_no_selector '#order_expiration_date'
+
+        select 'Credit card', from: 'Pay type'
+
+        assert_selector '#order_credit_card_number'
+        assert_selector '#order_expiration_date'
+    end
+
+    test 'Cart is not displayed when empty' do
+        visit store_index_url
+
+        assert_selector '.carts', visible: false
+    end
+
+    test 'Cart is displayed after adding a product' do
+        visit store_index_url
+
+        click_on 'Add to Cart', match: :first
+
+        assert_selector '.carts'
+    end
+
+    test 'Item in cart is highlighted after adding to cart' do
+        visit store_index_url
+
+        click_on 'Add to Cart', match: :first
+
+        assert_selector '.line-item-highlight'
     end
 end
