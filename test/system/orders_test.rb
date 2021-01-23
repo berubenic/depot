3fc0706388e2b25b27448eb4028823d700e2b11e
Nicholas <berubenic@gmail.com>
2021-01-23 09:54:35 -0500

    Chapter 14 end

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -1,83 +1,110 @@
 require 'application_system_test_case'
 
 class OrdersTest < ApplicationSystemTestCase
-    setup do
-        @order = orders(:one)
+  include ActiveJob::TestHelper
+  setup do
+    @order = orders(:one)
+  end
+
+  test 'visiting the index' do
+    visit orders_url
+    assert_selector 'h1', text: 'Orders'
+  end
+
+  test 'destroying a Order' do
+    visit orders_url
+    page.accept_confirm do
+      click_on 'Destroy', match: :first
     end
 
-    test 'visiting the index' do
-        visit orders_url
-        assert_selector 'h1', text: 'Orders'
+    assert_text 'Order was successfully destroyed'
+  end
+
+  test 'check routing number' do
+    LineItem.delete_all
+    Order.delete_all
+
+    visit store_index_url
+
+    click_on 'Add to Cart', match: :first
+
+    click_on 'Checkout'
+
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
+
+    assert_no_selector '#order_routing_number'
+    assert_no_selector '#order_account_number'
+
+    select 'Check', from: 'Pay type'
+
+    assert_selector '#order_routing_number'
+    assert_selector '#order_account_number'
+
+    fill_in 'Routing #', with: '123456'
+    fill_in 'Account #', with: '987654'
+
+    perform_enqueued_jobs do
+      click_button 'Place Order'
     end
 
-    test 'destroying a Order' do
-        visit orders_url
-        page.accept_confirm do
-            click_on 'Destroy', match: :first
-        end
+    orders = Order.all
+    assert_equal 1, orders.size
 
-        assert_text 'Order was successfully destroyed'
-    end
+    order = orders.first
 
-    test 'check routing number' do
-        visit store_index_url
+    assert_equal 'Dave Thomas', order.name
+    assert_equal '123 Main Street', order.address
+    assert_equal 'dave@example.com', order.email
+    assert_equal 'Check', order.pay_type
+    assert_equal 1, order.line_items.size
 
-        click_on 'Add to Cart', match: :first
+    mail = ActionMailer::Base.deliveries.last
+    assert_equal ['dave@example.com'], mail.to
+    assert_equal 'Nicholas <nicholas@depot.com>', mail[:from].value
+    assert_equal 'Pragmatic Store Order Confirmation', mail.subject
+  end
 
-        click_on 'Checkout'
+  test 'check credit card number' do
+    visit store_index_url
 
-        fill_in 'order_name', with: 'Dave Thomas'
-        fill_in 'order_address', with: '123 Main Street'
-        fill_in 'order_email', with: 'dave@example.com'
+    click_on 'Add to Cart', match: :first
 
-        assert_no_selector '#order_routing_number'
-        assert_no_selector '#order_account_number'
+    click_on 'Checkout'
 
-        select 'Check', from: 'Pay type'
+    fill_in 'order_name', with: 'Dave Thomas'
+    fill_in 'order_address', with: '123 Main Street'
+    fill_in 'order_email', with: 'dave@example.com'
 
-        assert_selector '#order_routing_number'
-        assert_selector '#order_account_number'
-    end
+    assert_no_selector '#order_credit_card_number'
+    assert_no_selector '#order_expiration_date'
 
-    test 'check credit card number' do
-        visit store_index_url
+    select 'Credit card', from: 'Pay type'
 
-        click_on 'Add to Cart', match: :first
+    assert_selector '#order_credit_card_number'
+    assert_selector '#order_expiration_date'
+  end
 
-        click_on 'Checkout'
+  test 'Cart is not displayed when empty' do
+    visit store_index_url
 
-        fill_in 'order_name', with: 'Dave Thomas'
-        fill_in 'order_address', with: '123 Main Street'
-        fill_in 'order_email', with: 'dave@example.com'
+    assert_selector '.carts', visible: false
+  end
 
-        assert_no_selector '#order_credit_card_number'
-        assert_no_selector '#order_expiration_date'
+  test 'Cart is displayed after adding a product' do
+    visit store_index_url
 
-        select 'Credit card', from: 'Pay type'
+    click_on 'Add to Cart', match: :first
 
-        assert_selector '#order_credit_card_number'
-        assert_selector '#order_expiration_date'
-    end
+    assert_selector '.carts'
+  end
 
-    test 'Cart is not displayed when empty' do
-        visit store_index_url
+  test 'Item in cart is highlighted after adding to cart' do
+    visit store_index_url
 
-        assert_selector '.carts', visible: false
-    end
+    click_on 'Add to Cart', match: :first
 
-    test 'Cart is displayed after adding a product' do
-        visit store_index_url
-
-        click_on 'Add to Cart', match: :first
-
-        assert_selector '.carts'
-    end
-
-    test 'Item in cart is highlighted after adding to cart' do
-        visit store_index_url
-
-        click_on 'Add to Cart', match: :first
-
-        assert_selector '.line-item-highlight'
-    end
+    assert_selector '.line-item-highlight'
+  end
 end
